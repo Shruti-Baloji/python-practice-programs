a3aed0cf25772e8c102ea9e8cbeb28217f4cf6c7
Shruti Baloji <shruti.baloji@gmail.com>
2024-01-09 11:53:32 +0530

    m multiples of n where the multiple is not divisble by (n-1) and (n-2) and (n-3)

--- num_pat_gen.py
+++ num_pat_gen.py
@@ -1,7 +1,8 @@
 #number pattern generator
+n = int(input("Enter the number of lines"))
 num = 1
-for i in range(1,5):
+for i in range(1,(n+1)):
     for j in range(1,(i+1)):
-        print(num,end = '')
+        print(num,"",end = "")
         num += 1
     print("")
