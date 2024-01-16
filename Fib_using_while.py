e76e67ef205f5e8c9264d1490adbf7b8df567ca4
Shruti Baloji <shruti.baloji@gmail.com>
2024-01-16 13:35:37 +0530

    Removing unwanted lines

--- Fib_using_while.py
+++ Fib_using_while.py
@@ -1,16 +1,14 @@
 while True:
     first = 0
     next = 1
-    n = int(input("How many numbers do you want to print?")) #n = 3, 0 , 1, 1
+    n = int(input("How many numbers do you want to print?"))
     if n == 0:
        exit 
-   # elif n == 1:
-    #   print(first)
     else:
         print (first)
         if n > 1:
             print (next)
-            while (n > 2): #i = 1, current = 1
+            while (n > 2): 
                 current = first + next
                 first = next
                 next = current
